03b4f2bca536ed9d3ef6fde83bcf3c6746e40640
snowwind <duningbt@163.com>
2020-03-17 16:02:56 +0800

    提交

---
+++ README.md
@@ -0,0 +1,13 @@
+# @umi-material/ysy
+
+ysy components
+
+## Usage
+
+```sh
+umi block https://github.com//tree/master/ysy
+```
+
+## LICENSE
+
+MIT
---
+++ dist/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var duningtest;
+
+(function (duningtest) {
+  function dn1(msg) {
+    console.log(msg);
+  }
+
+  duningtest.dn1 = dn1;
+
+  function dn2(msg) {
+    console.log(msg);
+  }
+
+  duningtest.dn2 = dn2;
+})(duningtest || (duningtest = {}));
